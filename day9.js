5c400e7f96b4038242444afe79d3bc5cbf7bb3df
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2023-06-29 18:45:03 +0530

    2666. Allow One Function Call

---
+++ day9.js
@@ -0,0 +1,27 @@
+/**
+
+ * @param {Function} fn
+ * @return {Function}
+ */
+ var once = function(fn) {
+    let call = false;
+    let result;
+
+    return function(...args){
+        if(!call){
+            call = true;
+            result = fn(...args);
+            return result;
+        }else{
+            return undefined;
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
