46abd8988f15eb61fbc64c05741a15e7b79d0d25
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2023-07-05 23:44:32 +0530

    2695. Array Wrapper

---
+++ day14.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ */
+ var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce(
+        (n,a) => n + a, 0
+    );
+}
+
+ArrayWrapper.prototype.toString = function() {
+    return '[${String(nums)}]';
+}
